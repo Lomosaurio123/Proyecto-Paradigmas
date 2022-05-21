a0e61850828f9c4f1ad2fa40d5c9e1e592e970bf
Mauricio Gael Hernandez Lom <mhernandezl1605@alumno.ipn.mx>
2022-05-21 17:17:41 -0500

    Inicializando el proyecto

---
+++ src/test/java/com/proyectoggg/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoggg.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
